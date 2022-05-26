3ca5860d6e08492f1109d5b8abfb32f222145f35
olseul <seul9085@naver.com>
2022-05-26 21:19:14 +0900

    feat: main_activity

---
+++ app/src/main/java/com/korea/kusuk/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.korea.kusuk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText emailView,passwordView;
+    Button loginBtnView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        emailView = findViewById(R.id.email);
+        passwordView = findViewById(R.id.StudentID);
+        loginBtnView = findViewById(R.id.loginBtn);
+
+        loginBtnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailView.getText().toString();
+                String password = passwordView.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/korea/kusuk/MainActivity.java
+++ app/src/main/java/com/korea/kusuk/MainActivity.java
@@ -1,14 +1,50 @@
 package com.korea.kusuk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    private ListView listview ;
+    private NoticeListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Adapter 생성
+        adapter = new NoticeListAdapter();
+
+        // 리스트뷰 참조 및 Adapter 달기
+        listview = (ListView) findViewById(R.id.noticeListView);
+        listview.setAdapter(adapter);
+        //listview.setOnItemClickListener(listener);
+
+        adapter.addItem("제목1", "내용1");  //(제목 부분, 내용)
+        adapter.addItem("제목1", "내용1");
+        adapter.addItem("제목1", "내용1");
+        adapter.addItem("제목1", "내용1");
+        adapter.addItem("제목1", "내용1");
+        adapter.addItem("제목1", "내용1");
+
+        adapter.notifyDataSetChanged(); //어댑터의 변경을 알림.
     }
 }
